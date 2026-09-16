268fd39e8f2797432ce6bb665a54107d29460d8f
Miraj2018 <mdmiraj.paperles@gmail.com>
2026-09-16 12:19:23 +0600

    Fix mariadb connection string parsing

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import mariadb from "mariadb";
 
-const connectionString = process.env.DATABASE_URL || "";
+const connectionString = (process.env.DATABASE_URL || "").replace("mysql://", "mariadb://");
 
 // We configure mariadb pool using the connection string from environment
 const pool = mariadb.createPool(connectionString);
